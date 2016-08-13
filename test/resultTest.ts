bf90322048e71ddb2ba59a59c5e6029b0e7e37c4
dominikus1993 <dominikus1910@gmail.com>
2016-08-13 20:04:55 +0200

    added test wrap result method with message

--- test/resultTest.ts
+++ test/resultTest.ts
@@ -11,15 +11,15 @@ describe("wrap result", () => {
         });
     });
 
-    describe("wrap result with null", () => {
-        const result = wrapResult({a: "a"});
+    describe("wrap result with object", () => {
+        const result = wrapResult({ a: "a" });
 
         it("result should equal", () => {
-            expect(result).to.deep.equal({ isSuccess: true, isError: false, value: {a: "a"}, messages: [] });
+            expect(result).to.deep.equal({ isSuccess: true, isError: false, value: { a: "a" }, messages: [] });
         });
     });
 
-    describe("wrap result with object", () => {
+    describe("wrap result with null", () => {
         const result = wrapResult(null);
 
         it("result should equal", () => {
@@ -32,6 +32,22 @@ describe("wrap result", () => {
 
         it("result should equal", () => {
             expect(result).to.deep.equal({ isSuccess: false, isError: true, value: undefined, messages: [] });
+        });
+    });
+
+    describe("wrap result with null and message", () => {
+        const result = wrapResult(null, "error value is null");
+
+        it("result should equal", () => {
+            expect(result).to.deep.equal({ isSuccess: false, isError: true, value: null, messages: ["error value is null"] });
+        });
+    });
+
+    describe("wrap result with undefined and message", () => {
+        const result = wrapResult(undefined, "error value is undefined");
+
+        it("result should equal", () => {
+            expect(result).to.deep.equal({ isSuccess: false, isError: true, value: undefined, messages: ["error value is undefined"] });
         });
     });
 });
