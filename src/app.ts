0c0dee3e55ef9ec54a7601dfb466f4abab6ee51b
dominikus1993 <dominikus1910@gmail.com>
2016-08-13 19:14:19 +0200

    reformat code

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import Koa = require("koa");
 
 const app = new Koa();
 
-app.use(function *(ctx) {
+app.use(function* (ctx) {
     yield ctx;
 });
 
-app.use(function *(ctx, next) {
-    this.body = {a: "dupa"};
+app.use(function* (ctx, next) {
+    this.body = { a: "dupa" };
 });
 
 app.listen(3000);
