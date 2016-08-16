8efd595f2d8806e4501bc606ebac0494ea88b651
dominikus1993 <dominikus1910@gmail.com>
2016-08-16 21:59:07 +0200

    added user model

---
+++ src/user/model.ts
@@ -0,0 +1,19 @@
+import { Document, Schema, model } from "mongoose";
+import * as passportLocalMongoose from "passport-local-mongoose";
+export type Role = "User" | "Admin";
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    role: Role;
+}
+
+export const UserSchema = new Schema({
+    username: {index: {unique: true}, required: false, type: String},
+    password: { required: true, type: String},
+    role: {type: String, enum: ["User", "Admin"], default: "User"},
+    email: {index: {unique: true}, required: false, type: String},
+});
+
+export const UserModel = model<User>("User", UserSchema);
