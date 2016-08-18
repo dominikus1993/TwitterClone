9a211fb8e189f4c7941ebd5d210bac4ff765d310
dominikus1993 <dominikus1910@gmail.com>
2016-08-18 23:05:45 +0200

    added test for saving user

--- src/user/model.ts
+++ src/user/model.ts
@@ -11,12 +11,12 @@ export interface User extends Document {
 }
 
 export const UserSchema = new Schema({
-    fallowers: [{index: {unique: true}, required: false, ref: "User" , type: Schema._Types.ObjectId}],
+    fallowers: [{index: {unique: true}, required: false, ref: "User" , type: Schema.Types.ObjectId}],
     username: {index: {unique: true}, required: false, type: String},
     password: { required: true, type: String},
     role: {type: String, enum: ["User", "Admin"], default: "User"},
     email: {index: {unique: true}, required: false, type: String},
-    createdDate: {required: true, type: Schema._Types.Date}
+    createdDate: {required: true, type: Schema.Types.Date}
 });
 
 export const UserModel = model<User>("User", UserSchema);
--- test/userTest.ts
+++ test/userTest.ts
@@ -1,2 +1,33 @@
 import {expect} from "chai";
 import {isNullOrUndefined} from "../src/global/utils";
+import {databaseConfig} from '../src/global/config';
+import {IUserRepository, UserRepository} from "../src/user/repository";
+import {UserModel} from "../src/user/model";
+import * as mongoose from "mongoose";
+import { Document, Schema, model } from "mongoose";
+
+describe("user repository test", () => {
+    let repository: IUserRepository;
+    before((done) => {
+        mongoose.Promise = databaseConfig.promise;
+        mongoose.connect("mongodb://localhost/twitter-test").then(() => {
+            mongoose.connection.db.dropDatabase();
+            repository = new UserRepository(UserModel);
+            done();
+        });
+    });
+
+    describe("register user", () => {
+        it("should save user without error", (done) => {
+            repository.register({ email: "admin@admin.admin", username: "admin", password: "admin" }).then((fulfilled) => {
+                done()
+            }, (rejected?: any) => {
+                done(rejected);
+            });
+        });
+    });
+
+    after(() => {
+        mongoose.connection.close();
+    });
+});
