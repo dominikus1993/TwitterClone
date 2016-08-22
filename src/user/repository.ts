279b1f841c6724ecdf498d3b1c7545b147ef6799
dominikus1993 <dominikus1910@gmail.com>
2016-08-22 21:48:15 +0200

    small refactoring

--- src/user/repository.ts
+++ src/user/repository.ts
@@ -1,9 +1,11 @@
-import {encrypt} from "../global/utils";
+import {curr, encrypt} from "../global/utils";
 import {appConfig} from "../global/config";
 import {User} from "./model";
 import {Model} from "mongoose";
 import * as moment from "moment";
 import * as Promise from "bluebird";
+
+const encode = curr(encrypt)(appConfig.secret);
 
 export interface IUserRepository {
     register(data: { email: string, username: string, password: string }): Promise<User>;
@@ -19,14 +21,14 @@ export class UserRepository implements IUserRepository {
         return new this.userModel({
             email: data.email,
             username: data.username,
-            password: encrypt(appConfig.secret, data.password),
+            password: encode(data.password),
             createdDate: new Date(),
         }).save() as {} as Promise<User>;
     }
 
     public login(data: { username: string, password: string }): Promise<User> {
         return this.userModel
-            .findOne({ username: data.username, password: encrypt(appConfig.secret, data.password) })
+            .findOne({ username: data.username, password: encode(data.password) })
             .exec() as any as Promise<User>;
     }
 }
