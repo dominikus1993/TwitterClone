057462ba5d335813365a661f99d8a1215b3ee750
dominikus1993 <dominikus1910@gmail.com>
2016-08-22 21:38:12 +0200

    added token model

--- src/user/model.ts
+++ src/user/model.ts
@@ -10,13 +10,28 @@ export interface User extends Document {
     fallowers: User[];  
 }
 
+export interface Token extends Document {
+    user: string | User;
+    token: string;
+    createdDate: Date;
+    expiredDate: Date;
+}
+
+export const TokenSchema = new Schema({
+    user: {required: false, ref: "User" , type: Schema.Types.ObjectId},
+    token: { required: true, type: String},
+    createdDate: {required: true, type: Schema.Types.Date},
+    expiredDate: {required: true, type: Schema.Types.Date},
+});
+
 export const UserSchema = new Schema({
     fallowers: [{index: {unique: true}, required: false, ref: "User" , type: Schema.Types.ObjectId}],
     username: {index: {unique: true}, required: false, type: String},
     password: { required: true, type: String},
     role: {type: String, enum: ["User", "Admin"], default: "User"},
     email: {index: {unique: true}, required: false, type: String},
-    createdDate: {required: true, type: Schema.Types.Date}
+    createdDate: {required: true, type: Schema.Types.Date},
 });
 
 export const UserModel = model<User>("User", UserSchema);
+export const TokenModel = model<Token>("Token", TokenSchema);
