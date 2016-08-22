94514a3bed340623badefcedc8e7e72db57f152e
dominikus1993 <dominikus1910@gmail.com>
2016-08-22 21:42:52 +0200

    add function for token generation

--- src/global/utils.ts
+++ src/global/utils.ts
@@ -14,3 +14,8 @@ export function curr<T, U, Z>(fun: (par1: T, par2: U) => Z): (t: T) => (u: U) =>
 export function encrypt(secret: string, text: string): string {
     return crypto.MD5(text, secret).toString();
 }
+
+export function generateToken(){
+    const rand = () => Math.random().toString(36).substring(2);
+    return rand() + rand();
+}
--- test/global/utilsTest.ts
+++ test/global/utilsTest.ts
@@ -1,5 +1,5 @@
 import {expect} from "chai";
-import {encrypt, curr, isNullOrUndefined} from "../../src/global/utils";
+import {encrypt, curr, generateToken, isNullOrUndefined} from "../../src/global/utils";
 describe("utils test", () => {
     describe("isnullorundefined test", () => {
 
@@ -40,6 +40,7 @@ describe("utils test", () => {
             });
         });
     });
+
     describe("encrypt test", () => {
         describe("admin phrase encrypt test", () => {
             const result = encrypt("123456789", "admin")
@@ -47,5 +48,13 @@ describe("utils test", () => {
                 expect(result).to.eq("21232f297a57a5a743894a0e4a801fc3");
             });
         });
+    });
+
+    describe("generate Token", () => {
+            const result = generateToken();
+            it("result should be not null", () => {
+                expect(result).to.be.not.null;
+                expect(result).to.be.string;
+            });
     });
 });
