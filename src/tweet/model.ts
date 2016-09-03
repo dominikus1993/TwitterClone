3aeb376ea3bb19a8ba91d56a818ae0018eef64e2
dominikus1993 <dominikus1910@gmail.com>
2016-09-03 13:12:20 +0200

    add new properties to tweet model

--- src/tweet/model.ts
+++ src/tweet/model.ts
@@ -6,12 +6,15 @@ interface Tweet extends Document {
     author: string | User;
     createdDate: Date;
     answers: string[] | Tweet[];
+    resenders: string[] | User[];
+    likers: string[] | User[];
 }
 
 export const TweetSchema = new Schema({
     answers: [{ref: "Tweet", type: Schema.Types.ObjectId}],
     author:  {ref: "User", type: Schema.Types.ObjectId},
     createdDate: {required: true, type: Schema.Types.Date},
+    likers: [{ref: "User", type: Schema.Types.ObjectId}],
     message: String,
     resenders: [{ref: "User", type: Schema.Types.ObjectId}],
 });
