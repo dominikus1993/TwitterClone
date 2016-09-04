465c8cea445207484c9d0836dd0cb5316656bc9c
dominikus1993 <dominikus1910@gmail.com>
2016-09-04 13:10:55 +0200

    add tag and like to tweet model

--- src/tweet/model.ts
+++ src/tweet/model.ts
@@ -1,20 +1,34 @@
 import { User } from "../user/model";
 import { Document, Schema, model } from "mongoose";
 
+type LikeType = "LIKE" | "UNLIKE";
+
+interface Tag extends Document {
+    name: string;
+    tweets: string[] | Tweet[];
+}
+
+interface Like extends Document {
+    user: string | User;
+    date: Date;
+    likeType: LikeType;
+}
+
 interface Tweet extends Document {
     message: string;
     author: string | User;
     createdDate: Date;
     answers: string[] | Tweet[];
     resenders: string[] | User[];
-    likers: string[] | User[];
+    likes: string[] | Like[];
+    tags: string[] | Tag[];
 }
 
 export const TweetSchema = new Schema({
     answers: [{ref: "Tweet", type: Schema.Types.ObjectId}],
     author:  {ref: "User", type: Schema.Types.ObjectId},
     createdDate: {required: true, type: Schema.Types.Date},
-    likers: [{ref: "User", type: Schema.Types.ObjectId}],
+    likes: [{ref: "User", type: Schema.Types.ObjectId}],
     message: String,
     resenders: [{ref: "User", type: Schema.Types.ObjectId}],
 });
