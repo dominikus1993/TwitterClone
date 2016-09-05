5558d5008aa9387f643f805c44c6209fb1091ee0
dominikus1993 <dominikus1910@gmail.com>
2016-09-05 12:15:18 +0200

    small changes

--- src/global/routing.ts
+++ src/global/routing.ts
@@ -1,3 +1,12 @@
-/**
- * Created by domin on 05.09.2016.
- */
+import {UserController} from  "../user/controller";
+import {Router} from "express";
+
+export function setRouting(router: Router) {
+    router.get("/*", (req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+        res.header("Access-Control-Allow-Credentials", "true");
+        next(); // http://expressjs.com/guide.html#passing-route control
+    });
+}
