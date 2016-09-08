46a480c5d50cd1328bf39beff9b36adb27bbd40d
dominikus1993 <dominikus1910@gmail.com>
2016-09-08 18:24:24 +0200

    add after test clause

--- test/user/controllerTest.ts
+++ test/user/controllerTest.ts
@@ -13,46 +13,62 @@ test.before("set mongodb", () => {
     return mongoose.connect("mongodb://localhost/twitter-test-controller").then(() => {
         mongoose.connection.db.dropDatabase();
         Object.defineProperty(Error.prototype, "toJSON", errorConfig);
+        // noinspection TypeScriptValidateTypes
         app.use(bodyParser.json());
+        // noinspection TypeScriptValidateTypes
         app.use(bodyParser.urlencoded({extended: true}));
+        // noinspection TypeScriptValidateTypes
         app.use("/test/api", router);
     });
 });
 
 test("register:Success", async(t) => {
+    const userForRegistration = {
+        email: "dominikus@test.test",
+        password: "admin",
+        passwordConfirm: "admin",
+        username: "dominikus1993",
+    };
     const res = await request(app)
         .post("/test/api/user/register")
-        .send({email: "dominikus@test.test", password: "admin", passwordConfirm: "admin", username: "dominikus1993"});
+        .send(userForRegistration);
 
     t.is(res.status, 200);
     t.truthy(res.body.isSuccess);
 });
 
 test("register:Error", async(t) => {
+    const userForRegistration = {
+        email: "dominikus@test.test2",
+        password: "admin",
+        passwordConfirm: "admin2",
+        username: "dominikus19932",
+    };
     const res = await request(app)
         .post("/test/api/user/register")
-        .send({
-            email: "dominikus@test.test2",
-            password: "admin",
-            passwordConfirm: "admin2",
-            username: "dominikus19932",
-        });
+        .send(userForRegistration);
 
     t.is(res.status, 404);
     t.falsy(res.body.isSuccess);
 });
 
 test("login:Success", async(t) => {
+    const userForRegistration = {
+        email: "loginTest@Test.test",
+        password: "admin",
+        passwordConfirm: "admin",
+        username: "loginTest",
+    };
     const registerResult = await request(app)
         .post("/test/api/user/register")
-        .send({email: "loginTest@Test.test", password: "admin", passwordConfirm: "admin", username: "loginTest"});
+        .send(userForRegistration);
 
     t.is(registerResult.status, 200);
     t.truthy(registerResult.body.isSuccess);
 
     const loginResult = await request(app)
         .post("/test/api/user/login")
-        .send({password: "admin", username: "loginTest"});
+        .send({password: userForRegistration.password, username: userForRegistration.username});
     t.is(loginResult.status, 200);
     t.truthy(loginResult.body.isSuccess);
 });
@@ -64,4 +80,8 @@ test("login:Error", async(t) => {
 
     t.is(res.status, 404);
     t.falsy(res.body.isSuccess);
+});
+
+test.after("close connection to database", () => {
+    mongoose.connection.close();
 });
